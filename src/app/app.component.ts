a24a0e34b167eb003ec768d500f5b5a54fa990b0
Dennis Mullen <dkmullen@gmail.com>
2020-12-30 13:56:04 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ HttpClient ]
 })
-export class AppComponent {
-  title = 'agendas';
+export class AppComponent implements OnInit {
+  data = {};
+  agendas = [];
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.http.get('https://4s6zwr22a0.execute-api.us-east-2.amazonaws.com/default/AgendaScraper')
+      .subscribe(res => {
+        this.data = (JSON.parse(res['body']))
+      for (let item in this.data) {
+        this.agendas.push(this.data[item]);
+      }
+    })
+  }
+
 }
